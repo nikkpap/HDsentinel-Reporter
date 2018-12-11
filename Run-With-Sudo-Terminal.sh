2f548c1cb3a1e502749fe21cd9e4083a5b780469
Nikolaos Pap <nikkpap@gmail.com>
2018-12-11 22:22:00 +0200

    Add files via upload

---
+++ Run-With-Sudo-Terminal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo -H ./HDSentinel -html -r 
+sudo -H ./HDSentinel -r 
+sudo -H chown $(whoami):$(whoami) report.*
+mkdir reports_bak/
+mv -f report.html report_$(date +"%m_%d_%Y").html
+mv -f report.txt report_$(date +"%m_%d_%Y").txt
+if which google-chrome >/dev/null; then
+   google-chrome report_$(date +"%m_%d_%Y").html
+else
+    firefox report_$(date +"%m_%d_%Y").html
+fi
+sudo -H mv -f report*.* reports_bak/
+cd reports_bak/
+sudo -H chown $(whoami):$(whoami) report.*
+clear
+exit 0
